258f7f476783a33bc625e636398bb389e5cc8479
unknown <631320232@SALA701-21.fspoaeduc.com.br>
2015-08-06 19:45:09 -0300

    Inclusao

---
+++ BDII/05082015.sql
@@ -0,0 +1,104 @@
+﻿/*Limpar tabelas*/
+drop table Xitensvenda;
+drop table Xvenda;
+drop table Xcliente;
+drop table Xproduto;
+drop table Xtipospagamento;
+
+/*Criação das tabelas*/
+create table Xproduto(
+	 codproduto int not null
+	,descricaoproduto varchar(50) not null
+	,unidade char(2) not null
+	,preco float not null
+	--Chave primária - codproduto
+);
+
+alter table Xproduto
+  add primary key (codproduto);
+
+create table Xcliente(
+	codcliente int not null
+	,cliente varchar(50) not null
+	,cpf char(11) not null
+	,endereco char(30) not null
+	--chave primária - codcliente
+);
+
+alter table Xcliente
+  add primary key (codcliente);
+
+create table Xtipospagamento(
+	codtppagamento int not null
+	,descricaotppagamento varchar(20) not null
+	--chave primária - codtppagamento
+);
+
+alter table Xtipospagamento
+  add primary key (codtppagamento);
+
+create table Xvenda(
+	nnf int not null
+	,dtvenda date not null
+	,codcliente int not null
+	,codtppagamento int not null
+	,vlvenda float not null
+	--chave primária – nnf, dtvenda
+	--chave estrangeira – codcliente da tabela de cliente
+	--chave estrangeira – codtppagamento da tabela de tipospagamento
+);
+
+alter table Xvenda
+  add primary key (nnf, dtvenda);
+
+alter table Xvenda
+  add foreign key (codcliente) references  Xcliente;
+
+alter table Xvenda
+  add foreign key (codtppagamento) references Xtipospagamento;
+
+create table Xitensvenda(
+	nnf int not null
+	,dtvenda date not null
+	,codproduto int not null
+	,qtde float not null
+	--chave primária – nnf, dtvenda, codproduto
+	--chave estrangeira – nnf, dtvenda da tabela de venda
+	--chave estrangeira – codproduto da tabela de produt
+);
+
+alter table Xitensvenda
+  add primary key ( nnf, dtvenda, codproduto);
+
+alter table Xitensvenda
+  add foreign key (nnf, dtvenda) references Xvenda;
+
+alter table Xitensvenda
+  add foreign key (codproduto) references Xproduto;
+
+
+/*Popular dados na base - Inicio*/
+insert into Xproduto values (1, 'Coca Cola', 'lt', 1.20);
+insert into Xproduto values (2, 'Presunto Sadia', 'kg', 5.40);
+insert into Xproduto values (3, 'Sabonete Palmolive', 'un', 0.65);
+insert into Xproduto values (4, 'Shampoo Colorama', 'un', 2.60);
+insert into Xproduto values (5, 'Cerveja Skol', 'gf', 0.99);
+insert into Xcliente values (1, 'Joao da Silva', '123456789', 'Rua Andradas, 250');
+insert into Xcliente values (2, 'Maria do Rosario', '26547899', 'Rua Lima e Silva, 648');
+insert into Xcliente values (3, 'Paulo Silveira', '8963254', 'Rua Plinio Brasil Milano, 980');
+insert into Xcliente values (4, 'Rosa Aparecida dos Santos', '5896332123', 'Av Ipiranga, 8960');
+insert into Xtipospagamento values (1, 'Cheque');
+insert into Xtipospagamento values (2, 'Dinheiro');
+insert into Xtipospagamento values (3, 'Crediario');
+insert into Xvenda values (1, '20/04/2002', 1, 1, 15.00);
+insert into Xvenda values (2, '20/04/2002', 2, 1, 7.50);
+insert into Xvenda values (1, '25/04/2002', 3, 2, 7.90);
+insert into Xvenda values (1, '30/04/2002', 3, 2, 8.50);
+insert into Xitensvenda values (1, '20/04/2002', 1, 1);
+insert into Xitensvenda values (1, '20/04/2002', 2, 2);
+insert into Xitensvenda values (2, '20/04/2002', 1, 3);
+insert into Xitensvenda values (2, '20/04/2002', 2, 2);
+insert into Xitensvenda values (2, '20/04/2002', 4, 4);
+insert into Xitensvenda values (1, '25/04/2002', 3, 9);
+insert into Xitensvenda values (1, '30/04/2002', 3, 7);
+/*Popular dados na base - fim*/
---
+++ BDII/05082015_query.sql
@@ -0,0 +1,107 @@
+﻿/*
+1 – Selecionar o nome do cliente e quantidade de produtos comprados, somente para clientes que compraram Coca Cola.
+2 – Selecionar o nome do cliente e o valor total comprado por ele.
+3 – Selecionar a descrição e o maior preço de produto vendido.
+4 – Selecionar o nome do cliente e descrição do tipo de pagamento utilizado nas vendas.
+5 – Selecionar o nome do cliente, nnf, data da venda, descrição do tipo de pagamento, descrição do produto e quantidade vendida dos itens vendidos.
+6 – Selecionar a média de preço dos produtos vendidos.
+7 – Selecionar o nome do cliente e a descrição dos produtos comprados por ele. Não repetir os dados (distinct)
+8 – Selecionar a descrição do tipo de pagamento, e a maior data de venda que utilizou esse tipo de pagamento. Ordenar a consulta pela descrição do tipo de pagamento.
+9 – Selecionar a data da venda e a média da quantidade de produtos vendidos. Ordenar pela data da venda decrescente.
+10 – Selecionar a descrição do produto e a média de quantidades vendidas do produto. Somente se a média for superior a 4
+*/
+
+--Query 01
+select cli.CLiente
+    , (select sum(its2.qtde) 
+	 from xItensVenda its2 
+        where its2.nnf = vd.nnf 
+          and its2.dtVenda = vd.dtVenda 
+      )as qtdProduto
+  from xCliente cli
+inner join xVenda vd on vd.CodCliente = cli.codcliente
+inner join xItensVenda its on its.nnf = vd.nnf
+			  and its.dtVenda = vd.dtVenda
+inner join xProduto pd on pd.CodProduto = its.CodProduto
+		      and upper(pd.DescricaoProduto) like Upper('%COCA COLA%')
+
+--Query 02
+
+select cli.CLiente, sum(vd.VlVenda)
+  from xCliente cli
+inner join xVenda vd on vd.CodCliente = cli.codcliente
+group by cli.CLiente
+
+--Query 03
+
+select pd.DescricaoProduto ,pd.Preco
+  from xVenda vd
+ inner join xItensVenda its on its.nnf = vd.nnf
+			  and its.dtVenda = vd.dtVenda
+ inner join xProduto pd on pd.CodProduto = its.CodProduto
+ group by pd.DescricaoProduto,pd.preco
+ having pd.preco = (select max(pd1.Preco)from xproduto pd1)
+
+--Query 04
+
+select cli.Cliente, tp.DescricaotpPagamento
+  from xCliente cli
+inner join xVenda vd on vd.CodCliente = cli.codcliente
+inner join xTiposPagamento tp on tp.codtpPagamento = vd.codTpPagamento 
+		
+
+--query 05
+
+select cli.Cliente
+      ,vd.nnf
+      ,vd.dtVenda
+      ,tp.DescricaotpPagamento
+      ,pd.DescricaoProduto
+      ,its.qtde
+  from xCliente cli
+inner join xVenda vd on vd.CodCliente = cli.codcliente
+inner join xItensVenda its on its.nnf = vd.nnf
+			  and its.dtVenda = vd.dtVenda
+inner join xProduto pd on pd.CodProduto = its.CodProduto
+inner join xTiposPagamento tp on tp.codtpPagamento = vd.codTpPagamento
+group by cli.Cliente
+	,vd.nnf
+	,vd.dtVenda
+	,tp.DescricaotpPagamento
+	,pd.DescricaoProduto 
+	,its.qtde
+order by cli.Cliente
+	,vd.nnf
+	,vd.dtVenda
+	,tp.DescricaotpPagamento
+	,pd.DescricaoProduto 
+
+--query 06
+
+select round(cast( avg(pd.Preco) as numeric),2) as media
+  from xCliente cli
+inner join xVenda vd on vd.CodCliente = cli.codcliente
+inner join xItensVenda its on its.nnf = vd.nnf
+			  and its.dtVenda = vd.dtVenda
+inner join xProduto pd on pd.CodProduto = its.CodProduto
+
+--Query 07
+select distinct cli.Cliente
+      ,pd.DescricaoProduto
+ from xCliente cli
+inner join xVenda vd on vd.CodCliente = cli.codcliente
+inner join xItensVenda its on its.nnf = vd.nnf
+			  and its.dtVenda = vd.dtVenda
+inner join xProduto pd on pd.CodProduto = its.CodProduto
+group by cli.Cliente
+	,pd.DescricaoProduto 
+order by cli.Cliente
+
+
+--query 08
+select tp.DescricaotpPagamento,vd.DtVenda
+  from xVenda vd
+inner join xTiposPagamento tp on tp.codtpPagamento = vd.codTpPagamento		
+group by tp.DescricaotpPagamento,vd.DtVenda
+having vd.DtVenda = (select max(vd1.dtVenda) from xvenda vd1 where vd1.codtppagamento =  vd.codtppagamento)
+
