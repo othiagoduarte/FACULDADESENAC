501ad3cba9335462361182c038b5c4b6beb6c3f0
= <=>
2015-08-26 21:54:43 -0300

    BD2 - Atualizacao

--- BDII/scripts/Trabalho_01.sql
+++ BDII/scripts/Trabalho_01.sql
@@ -9,30 +9,18 @@ $$
 		V_MultaTotal Decimal;
 		V_VelocidadeApurada Decimal;
 		V_Pontos Integer;
+		V_Infracao varchar(10);
 		
 	Begin		
-		--Teste 
-		V_NomeMotorista:= 'Thiago Duarte';
-		--Teste 
 		V_VelocidadeApurada:= P_velocidade * 0.90; -- 90% da velocidade apurada
+		V_pontos:= 0;
+		V_Multa:= 0;
 		
-		--Calcular valor da multa	
-		
-		Case 
-		
-			When V_VelocidadeApurada Between 80.01 And 110 Then
-				V_Multa:= 120;	
-				V_Pontos:=20;
-			When V_VelocidadeApurada Between 110.01 And 140 Then
-				V_Multa:= 350;	
-				V_Pontos:=40;
-			When V_VelocidadeApurada > 140 Then
-				V_Multa:= 680;
-				V_Pontos:=60;
-			Else
-				V_Multa:= 0;
-				V_Pontos:=0;	
-		End Case;
+                select c.Pontos, c.valorMulta , c.Infracao
+		  into V_pontos,V_Multa ,V_Infracao
+                  from ex_excessovelocidade e
+		 inner join ex_categoriaMulta c on c.Infracao = e.Infracao
+		  where V_VelocidadeApurada between  e.VelocidadeInicial and e.VelocidadeFinal;
 		
 		If V_Multa > 0 Then
 			
@@ -40,8 +28,8 @@ $$
 			  From ex_motorista
 			 Where Cnh = P_cnh;
 
-			Insert Into ex_multa(cnh,velocidadeApurada,velocidadeCalculada,pontos,valor)
-			Values(P_cnh, P_velocidade,V_VelocidadeApurada,V_pontos,V_Multa); 
+			Insert Into ex_multa(cnh,velocidadeApurada,velocidadeCalculada,pontos,valor,Infracao)
+			Values(P_cnh, P_velocidade,V_VelocidadeApurada,V_pontos,V_Multa,V_Infracao); 
 			
 			Select Sum(Pontos)
 			  Into V_MultaTotal 
@@ -55,5 +43,75 @@ $$
 		Return V_Mensagem;
 
 	End;
+$$ 
+Language PlpgSQL;
+Create or Replace Function FN_AtualiarTotalMultas() 
+Returns Varchar(100) AS 
+$$
+	Declare
+	
+	V_Mensagem varchar(100);
+	V_Cursor RECORD; 
+	
+	Begin		
+				
+		for V_Cursor in (select Mo.CNH , sum( COALESCE(mu.valor,0) ) as Total   
+		                   from ex_motorista mo
+                                   Left join ex_multa mu on mu.CNH = mo.CNH
+                                  group by Mo.CNH 
+				)
+		loop
+			begin
+			
+				update ex_motorista
+				  set totalMultas = V_Cursor.Total
+				where cnh = V_Cursor.cnh; 
+
+			End;
+			
+		end loop;
+		
+		V_Mensagem:= 'Atualização realizada com sucesso';
+				
+		Return V_Mensagem;
+	
+	End;
+$$ 
+Language PlpgSQL;
+
+Create or Replace Function FN_ConsultaPontosValor() 
+Returns setof  record AS 
+$$
+begin 
+return query 	
+select mu.Infracao, sum(valor) totalPontos
+  from ex_motorista mo
+ inner join ex_Multa mu on mu.CNH = mo.CNH
+ group by mu.Infracao;
+	
+
+end	
+
+	
+$$ 
+Language PlpgSQL;
+
+
+
+
+Create or Replace Function FN_ConsultaInfracaoPontosValor() 
+Returns setof  record AS 
+$$
+
+begin 
+	return query 
+         select Count(*) as TotalInfracao,mu.Infracao, DataInfracao,sum(pontos) totalPontos
+	   from ex_motorista mo
+	   inner join ex_Multa mu on mu.CNH = mo.CNH
+	   group by mu.Infracao, DataInfracao;
+	
+
+end
+
 $$ 
 Language PlpgSQL;
